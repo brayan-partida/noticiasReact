989c06c501820a1cc7a5e9e61a345b6845e8f1d6
brayan-partida <brayan.ramirez.partida@gmail.com>
2021-03-10 22:00:10 -0800

    build5

--- src/components/Noticias.js
+++ src/components/Noticias.js
@@ -1,20 +1,50 @@
 import React, { useState, Fragment, useEffect } from "react";
 import styles from "./noticias.module.css";
+import nextId from "react-id-generator";
 const Noticias = ({ noticiasall }) => {
   //if (noticias === "") return null;
   const { urlToImage, url, description, title } = noticiasall;
+  var htmlId = nextId();
+  var htmlId2 = nextId();
+  var htmlId3 = nextId();
+  var htmlId4 = nextId();
+  var htmlId5 = nextId();
+  var htmlId6 = nextId();
 
   return (
     <Fragment>
       <div className="row">
         {noticiasall.map((noticia) => (
-          <div key={noticia.url} className={`card-image styles.noticias_image`}>
-            <img
-              key={noticia.url}
-              src={noticia.urlToImage}
-              className={styles.imagen_noticia}
-            />
-          </div>
+          <Fragment>
+            <div key={htmlId} className={`col s12 m6 l4 styles.noticias_image`}>
+              <div key={htmlId2} className="card">
+                <div
+                  key={htmlId3}
+                  className={`card-image styles.noticias_image`}
+                >
+                  <img
+                    key={noticia.url}
+                    src={noticia.urlToImage}
+                    className={styles.imagen_noticia}
+                  />
+                </div>
+                <div key={htmlId4} className="card-content">
+                  <h5>{noticia.title}</h5>
+                  <p key={noticia.url}>{noticia.description}</p>
+                </div>
+
+                <div key={htmlId5} className="card-action">
+                  <a
+                    key={htmlId6}
+                    className="waves-effect waves-light btn-small"
+                    href={noticia.url}
+                  >
+                    ir ala noticia
+                  </a>
+                </div>
+              </div>
+            </div>
+          </Fragment>
         ))}
       </div>
     </Fragment>
